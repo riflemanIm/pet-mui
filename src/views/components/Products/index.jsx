8fd24d71e07367bf537355439e502059e2383499
Oleg La <oleglambin@gmail.com>
2025-05-06 17:01:22 +0300

    dev: catalog

---
+++ src/views/components/Products/index.jsx
@@ -0,0 +1,107 @@
+import { Card } from "@mui/material";
+import Grid2 from "@mui/material/Grid2";
+import footerRoutes from "assets/footer.routes";
+import bgImage from "assets/images/bg-about-us.jpg";
+import routes from "assets/routes";
+import Container from "components/Container";
+import DefaultFooter from "components/Footers/DefaultFooter";
+import MKBox from "components/MKBox";
+import MKButton from "components/MKButton";
+import MKTypography from "components/MKTypography";
+import DefaultNavbar from "components/Navbars/DefaultNavbar";
+
+import Products from "./Products";
+
+const IndexProducts = () => {
+  return (
+    <>
+      <DefaultNavbar
+        routes={routes}
+        action={{
+          type: "external",
+          route: "/catalog",
+          label: "Каталог",
+          color: "default",
+        }}
+        transparent
+        light
+      />
+      <MKBox
+        minHeight="35vh"
+        width="100%"
+        sx={{
+          backgroundImage: ({
+            functions: { linearGradient, rgba },
+            palette: { gradients },
+          }) =>
+            `${linearGradient(
+              rgba(gradients.dark.main, 0.6),
+              rgba(gradients.dark.state, 0.6)
+            )}, url(${bgImage.src})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "grid",
+          placeItems: "center",
+        }}
+      >
+        <Container>
+          <Grid2
+            container
+            size={{ xs: 12, lg: 8 }}
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+            sx={{ mx: "auto", textAlign: "center" }}
+          >
+            <MKTypography
+              variant="h1"
+              color="white"
+              sx={({ breakpoints, typography: { size } }) => ({
+                [breakpoints.down("md")]: {
+                  fontSize: size["3xl"],
+                },
+              })}
+            >
+              Work with an amazing design
+            </MKTypography>
+            <MKTypography
+              variant="body1"
+              color="white"
+              opacity={0.8}
+              mt={1}
+              mb={3}
+            >
+              We&apos;re constantly trying to express ourselves and actualize
+              our dreams. If you have the opportunity to play this game
+            </MKTypography>
+            <MKButton
+              color="default"
+              sx={{ color: ({ palette: { dark } }) => dark.main }}
+            >
+              create account
+            </MKButton>
+           
+          </Grid2>
+        </Container>
+      </MKBox>
+      <Card
+        sx={{
+          p: 2,
+          mx: { xs: 2, lg: 3 },
+          mt: -8,
+          mb: 4,
+          boxShadow: ({ boxShadows: { xxl } }) => xxl,
+        }}
+      ><Container>
+        <Products />
+        </Container>
+      </Card>
+
+      <MKBox pt={6} px={1} mt={6}>
+        <DefaultFooter content={footerRoutes} />
+      </MKBox>
+    </>
+  );
+};
+
+export default IndexProducts
