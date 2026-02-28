f3adf329bf4befafbad9d69730152200906dd3f3
Oleg La <oleglambin@gmail.com>
2026-02-28 16:26:34 +0300

    fix: ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
     {
       name: "s-pet",
       script: "node_modules/next/dist/bin/next",
-      args: "start -p 3000",
+      args: "start -p 8080",
       cwd: "/mnt/disk-1/www/s-pet.ru",
       env: { NODE_ENV: "production" },
     },
